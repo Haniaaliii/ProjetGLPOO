1fb4034c10bb79185940e6f10c57923fc0bf7263
Haniaaliii <81871591+Haniaaliii@users.noreply.github.com>
2021-05-04 00:09:11 +0200

    Update MusicThread.java

--- ProjetGLPOO/src/client/musichub/sound/MusicThread.java
+++ ProjetGLPOO/src/client/musichub/sound/MusicThread.java
@@ -3,8 +3,7 @@ package client.musichub.sound;
 import javax.sound.sampled.AudioInputStream;
 
 /**
- * Thread utilisé dans le protocole d'écoute d'une musique
- * @author Haïdi
+ * 
  *
  */
 public class MusicThread implements Runnable {
